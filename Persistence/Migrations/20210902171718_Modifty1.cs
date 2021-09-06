8befd71195a38f6d49fcc61b3e74c7208b247c29
vprakashonnet <vprakashonnet@gmail.com>
2021-09-06 17:50:20 +0530

    End of Section 2

---
+++ Persistence/Migrations/20210902171718_Modifty1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class Modifty1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Venu",
+                table: "Activities",
+                newName: "Venue");
+
+            migrationBuilder.RenameColumn(
+                name: "Titile",
+                table: "Activities",
+                newName: "Title");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Venue",
+                table: "Activities",
+                newName: "Venu");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Activities",
+                newName: "Titile");
+        }
+    }
+}
